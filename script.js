61774df89ee87b6c21b99da342dc709d1b3394ef
Mike Sasiela <m5455y@gmail.com>
2020-11-24 21:50:53 -0600

    Fixed start button

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ function startCountdown() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      sendMessage();
+    //   sendMessage();
     }
 
   }, 1000);
 }
 
 // Gets ran when countdown times out
-function sendMessage() {
+// function sendMessage() {
 
-}
+// }
 
 
-//Event Listeners
-// Starts count down when start button is clicked
-startBtn.addEventListener('click', startCountdown())
+// Event Listeners
+// Starts countdown when start button is clicked
+startBtn.addEventListener('click', startCountdown)
