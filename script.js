df8463429b56d602848d387ae5a3c35f42e4f296
Mike Sasiela <m5455y@gmail.com>
2020-11-25 17:10:29 -0600

    Have guts of project. Working on getting questions do display

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Questions 
+var questions = [
+  {
+      title: "Commonly Used data types DO NOT include:",
+      choices: ["stings", "alerts", "booleans", "numbers"],
+      answer: "alerts"
+  },
+  {
+      title: "The condition in an if / else statment is enclosed within _____.",
+      choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+      answer: "parentheses"
+  },
+  {
+      title: "What javascipt method can we use to select an html element?",
+      choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+      answer: "Both 1 and 3"
+  },
+  {
+      title: "What html tag is NOT included in the HEAD tag?",
+      choices: ["link", "meta", "title", "header"],
+      answer: "header"
+  },
+  {
+      title: "What attribute is used in html to decorate content?",
+      choices: ["css", "class", "src", "style"],
+      answer: "style"
+  }
+]
+
 var countDown = document.querySelector('.countdown-timer')
 var startBtn = document.querySelector('.start-button')
 
@@ -12,18 +41,37 @@ function startCountdown() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-    //   sendMessage();
+    // endQuiz();
     }
 
   }, 1000);
 }
 
 // Gets ran when countdown times out
-// function sendMessage() {
+// function endQuiz() {
 
 // }
+
+// Start's Quiz
+function startQuiz() {
+  $('.start-button').attr("class", "hide")
+  $('#quiz-container').attr("class", "show")
+    getQustion()
+}
+
+//Get's Quesitons
+function getQustion() {
+  var currentQuestion = questions[currentQuestionIndex];
+
+  var questionText = $('#question-text')
+  questionText.textContent = currentQuestion.title
+
+}
 
 
 // Event Listeners
 // Starts countdown when start button is clicked
-startBtn.addEventListener('click', startCountdown)
+startBtn.addEventListener('click', () => {
+  startCountdown();
+  startQuiz();
+})
